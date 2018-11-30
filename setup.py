3df36b120855cc36e61001c97716520ed9290c00
Jeffrey Jackovich <JeffreyJackovich@users.noreply.github.com>
2018-11-30 03:33:02 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,4 +6,6 @@ setup(name='factual_data_to_postgresql',
       url='https://github.com/JeffreyJackovich/factual_data_to_postgresql',
       author='Jeffrey Jackovich',
       license='MIT',
-      packages=['factual_data_to_postgresql'])
+      packages=['factual_data_to_postgresql'],
+     install_requires=['psycopg2==2.7.5',
+                        'factual-api==1.7.0'])
