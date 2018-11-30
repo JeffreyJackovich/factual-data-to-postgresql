c98a1487bd036b928263cbbdf9bb0b03b2d52fcd
Jeffrey Jackovich <JeffreyJackovich@users.noreply.github.com>
2018-11-30 03:23:20 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='factual_data_to_postgresql',
+      version='0.1',
+      description='Extracts data from factual to Postgresql',
+      url='https://github.com/JeffreyJackovich/factual_data_to_postgresql',
+      author='Jeffrey Jackovich',
+      license='MIT',
+      packages=['factual_data_to_postgresql'])
